9d0035814dd9c532c321e7822368b66e0ca2fbca
Jan Kuri <jkuri88@gmail.com>
2017-04-15 11:29:11 +0200

    feat(pie-chart): pie chart transition

--- src/app/components/app-cpu-pie/app-cpu-pie.component.ts
+++ src/app/components/app-cpu-pie/app-cpu-pie.component.ts
@@ -12,11 +12,15 @@ export class AppCpuPieComponent implements OnInit {
   defs: any;
   data: any[];
   r: number
+  pie: any;
+  path: any;
+  pathEl: any;
+  arc: any;
 
   constructor(private elementRef: ElementRef) { }
 
   ngOnInit() {
-    this.pieChartEl = this.elementRef.nativeElement.querySelector('.pie-chart');
+    this.pieChartEl = this.elementRef.nativeElement.querySelector('.pie-chart-container');
     this.render();
   }
 
@@ -25,7 +29,7 @@ export class AppCpuPieComponent implements OnInit {
     let h = this.pieChartEl.clientHeight;
     this.r = 160;
 
-    this.svg = d3.select(this.pieChartEl).append('svg').attr('width', w).attr('height', h);
+    this.svg = d3.select(this.pieChartEl).select('.pie-chart').append('svg').attr('width', w).attr('height', h);
 
     this.g = this.svg.append('g')
       .attr('transform', 'translate(200, 200)');
@@ -33,49 +37,68 @@ export class AppCpuPieComponent implements OnInit {
     this.defs = this.svg.append('defs');
 
     this.data = [
-      {label:"Basic", color:"#3366CC"},
-      {label:"Plus", color:"#DC3912"},
-      {label:"Lite", color:"#FF9900"},
-      {label:"Elite", color:"#109618"},
-      {label:"Delux", color:"#990099"}
+      {label:"Basic", color:"#438FCC"},
+      {label:"Plus", color:"#88C340"},
+      {label:"Lite", color:"#DA031B"},
+      {label:'Aloha', color: '#6E7F9A'},
+      {label:"Elite", color:"#FFC356"},
+      {label:"Delux", color:"#65DBFF"},
+      {label:'Haloha', color: '#65DBFF'},
     ].map(d => {
       return { label: d.label, value: 1000 * Math.random(), color: d.color };
     });
 
-    let pie = d3.pie().sort(null).value((d: any) => d.value);
-    let path = d3.arc()
-      .outerRadius(this.r - 100)
+    this.pie = d3.pie().sort(null).value((d: any) => d.value);
+    this.arc = d3.arc()
+      .outerRadius(this.r)
       .innerRadius(0);
 
-    // let arc = this.g.selectAll('.arc')
-    //   .data(pie(this.data as any))
-    //   .enter().append('g')
-    //   .attr('class', 'arc');
-
-    // arc.append('path')
-    //   .attr('d', path)
-    //   .attr('fill', (d: any) => d.data.color);
-
-    let gPie = this.svg.append('g')
-      .attr('transform', 'translate(200, 200)');
+    let label = d3.arc()
+      .outerRadius(this.r - 40)
+      .innerRadius(this.r - 40);
 
     this.createGradients();
 
-    gPie.selectAll('.arc')
-      .data(pie(this.data as any))
+    this.path = this.svg.datum(this.data).selectAll('path')
+      .data(this.pie)
       .enter().append('path')
+      .attr('transform', 'translate(200, 200)')
       .attr('fill', (d: any, i: number) => `url(#pie-gradient-${i}`)
-      .attr('d', path)
-      .attr('transform', 'scale(3, 3)');
+      .attr('d', this.arc)
+      .each(function(d) { this._current = d; });
+
+    setTimeout(() => {
+      this.change();
+    }, 2000);
   }
 
-  createGradients() {
+  change = () => {
+    this.data = this.data.map(d => {
+      return { label: d.label, value: 1000 * Math.random(), color: d.color };
+    });
+
+    this.pie.value((d: any, i: number) => this.data[i].value);
+    this.path = this.path.data(this.pie);
+    this.path.transition().duration(2000).attrTween('d', arcTween);
+
+    let arc = this.arc;
+
+    function arcTween(a) {
+      let i = d3.interpolate(this._current, a);
+      this._current = i(0);
+      return function(t) {
+        return arc(i(t));
+      }
+    }
+  }
+
+  createGradients(): void {
     let gradient = this.defs.selectAll('.gradient')
       .data(this.data.map(d => d.color))
       .enter().append('radialGradient')
       .attr('id', (d: any, i: number) => `pie-gradient-${i}`)
       .attr('gradientUnits', 'userSpaceOnUse')
-      .attr('cx', 0).attr('cy', 0).attr('r', this.r).attr('spreadMethod', 'pad');
+      .attr('cx', 0).attr('cy', 0).attr('r', this.r + 200).attr('spreadMethod', 'pad');
 
     gradient.append('stop').attr('offset', '0%').attr('stop-color', (d: any) => d);
     gradient.append('stop').attr('offset', '30%')
